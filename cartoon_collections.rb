c10785c314678252bdc454d4a2b1c54505215429
Katie Ashlin <akka726@outlook.com>
2018-11-05 17:16:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  dwarves.each_with_index do |dwarves|
-    puts dwarves
+  dwarves.each_with_index do |dwarves, index|
+    puts "dwarves
 end
 end
 
