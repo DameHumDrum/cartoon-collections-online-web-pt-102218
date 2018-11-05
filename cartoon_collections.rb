cd04d224ab1c23e047517a3e1c7bf86bc68d11f9
Katie Ashlin <akka726@outlook.com>
2018-11-05 17:38:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(cheesey) # code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheesey.find do |cheese|
+    
 end
